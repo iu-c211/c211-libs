f39980732a5c267ede6cc5d560e3c881df76d351
JP Verkamp <me@jverkamp.com>
2013-10-22 16:12:53 -0700

    Actually saved file before uploading

--- c211-libs/tree.rkt
+++ c211-libs/tree.rkt
@@ -2,40 +2,55 @@
 
 (provide
  (contract-out
-  [draw-tree     (tree/c . -> . void?)]
-  [empty-tree    (-> tree/c)]
-  [empty-tree?   (any/c . -> . boolean?)]
-  [leaf          (any/c . -> . tree/c)]
-  [leaf?         (any/c . -> . boolean?)]
-  [left-subtree  (tree/c . -> . tree/c)]
-  [right-subtree (tree/c . -> . tree/c)]))
-  
-  
+  [draw-tree      (tree/c . -> . void?)]
+  [empty-tree     (-> tree/c)]
+  [empty-tree?    (any/c . -> . boolean?)]
+  [leaf           (any/c . -> . tree/c)]
+  [leaf?          (any/c . -> . boolean?)]
+  [left-subtree   (tree/c . -> . tree/c)]
+  [left-subtree?  (tree/c . -> . boolean?)]
+  [right-subtree  (tree/c . -> . tree/c)]
+  [right-subtree? (tree/c . -> . boolean?)]
+  [root-value     (tree/c . -> . any/c)]
+  [tree           (any/c tree/c tree/c . -> . tree/c)]
+  [tree?          (any/c . -> . boolean?)]))
 
+; To draw trees
 (require slideshow/pict)
 
-(define-struct empty-tree ())
-(define-struct tree (value left right))
+; Structures for trees
+(define-struct empty-tree () #:transparent)
+(define-struct tree (value left right) #:transparent)
 
+; Union contract so that trees can be empty or not
 (define tree/c (or/c empty-tree? tree?))
 
+; Create or test if something is a leaf (tree with no children)
 (define (leaf value) (tree value (empty-tree) (empty-tree)))
-
 (define (leaf? tr) 
   (and (tree? tr)
        (empty-tree? (tree-left tr))
        (empty-tree? (tree-right tr))))
 
-(define left-subtree tree-left)
-(define right-subtree tree-right)
+; Rename left and right subtree
+; A straight define or rename-out doesn't fix the contract error message
+(define (left-subtree tr) (tree-left tr))
+(define (right-subtree tr) (tree-right tr))
 
+; Check if the left/right subtree is non-empty
+(define (left-subtree? tr) (not (empty-tree? (left-subtree tr))))
+(define (right-subtree? tr) (not (empty-tree? (right-subtree tr))))
+
+; Get the root value of a tree
+(define (root-value tr) (tree-value tr))
+
+; Draw a tree using slideshow/pict
 (define (draw-tree tr)
   (match tr
     [(tree value left right)
      (define v (text (format "~a" value)))
      (define l (draw-tree left))
      (define r (draw-tree right))
-     
      (pin-line (pin-line (vc-append 10 v (ht-append l r))
                          v cb-find
                          l ct-find
@@ -44,9 +59,9 @@
                r ct-find
                #:color (if (empty-tree? right) "white" "black"))]
      [else
-     (colorize (rectangle 40 20) "white")]))
+     (colorize (rectangle 12 12) "white")]))
 
-(draw-tree
+(define tr
  (tree 8
        (tree 6 (leaf 7) (leaf 5))
        (tree 3 
