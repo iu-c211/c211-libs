4879a1be31a26f770d013e5acb5f703047258d42
Seth Kirby <mr.seth.kirby@gmail.com>
2014-02-17 21:19:20 -0500

    Made fast image the default, renamed image testing suites respectively

--- c211-libs/image-test.rkt
+++ c211-libs/image-test.rkt
@@ -41,6 +41,23 @@
 (check = 25 (image-cols i) "make-image: Image height")
 ))
 
+(cout "Image mutation and reference")
+(time
+(test-begin
+ (for ((r (in-range 0 (image-rows i))))
+   (for ((c (in-range 0 (image-cols i))))
+     (for ((band (in-range 0 3)))
+       (check = (image-ref i r c band) (image-ref ibl r c band))
+       (image-set! i r c band 5)
+       (check (lambda (x y) (not (= x y)))
+              (image-ref i r c band)
+              (image-ref ibl r c band))
+       (image-set! ibl r c band 5)
+       (check = (image-ref i r c band) (image-ref ibl r c band))
+       )))
+ (check (lambda (x y) (not (image-equal? x y))) i ibl))
+)
+
 (cout "Color equality")
 (time
  (test-begin
@@ -57,6 +74,36 @@
                (color-equal? (color r g b) (color rr rg rb))))))))
 
 (set! i (image-map (lambda (x) (color (random 256) (random 256) (random 256))) i)) 
+
+(cout "Image and color mutation, referencing, and equality testing")
+(time
+ (test-begin
+  (for ((count (in-range 0 2000)))
+    (let* ((r (random 50)) (c (random 25)) (clr (image-ref i r c)) (img (image-map (lambda (x) x) i)))
+      (let* ((band (random 3)) (newval (random 256)) (oldval (image-ref i r c band)))
+        (image-set! i r c band newval)
+        (check = oldval (color-ref clr band))
+        (check (lambda (x y) (not (xor x y)))
+               (color-equal? clr (image-ref i r c))
+               (image-equal? img i))
+        (color-set! clr band newval)
+        (check color-equal? clr (image-ref i r c))))))) 
+
+(cout "Image and list conversions")
+(time
+ (test-begin
+  (for ((times (in-range 0 1000)))
+    (let ((r (+ 1 (random 10)))(c (+ 1 (random 10))))
+      (let* ((img (make-image r c (color (random 256) (random 256) (random 256))))
+             (ls (image->list img)))
+        (check = (image-rows img) r)
+        (check = (image-cols img) c)
+        (check = (length ls) (* c r))
+        (define i 0)
+        (for ((clr ls))
+          (check color-equal? clr (image-ref img (quotient i c) (modulo i c)))
+          (set! i (+ i 1)))
+        (check image-equal? (list->image c ls) img))))))
           
 (cout "Advanced image creation and image reference")
 (time
@@ -70,11 +117,11 @@
         (check = r (image-ref img r c 0))
         (check = (+ c r) (image-ref img r c 'blue))
         (check = (+ r c) (image-ref img r c 2)))))
-  (for ((clr (image->color-list (make-image 25 35 (color 123 231 98)))))
-    (check color-equal? clr (color 123 231 98 12))
-    (check (lambda (x y) (not (equal? x y))) clr (color 123 231 98 12)))
-  (for ((clr (image->color-list (make-image 25 35))))
-    (check color-equal? clr (color 0 0 0 12))
-    (check (lambda (x y) (not (equal? x y))) clr (color 0 0 0 12)))))
+  (for ((clr (image->list (make-image 25 35 (color 123 231 98)))))
+    (check color-equal? clr (color-bytes (bytes 12 123 231 98)))
+    (check (lambda (x y) (not (equal? x y))) clr (color-bytes (bytes 12 123 231 98))))
+  (for ((clr (image->list (make-image 25 35))))
+    (check color-equal? clr (color-bytes (bytes 12 0 0 0)))
+    (check (lambda (x y) (not (equal? x y))) clr (color-bytes (bytes 12 0 0 0))))))
 
         
--- c211-libs/image.rkt
+++ c211-libs/image.rkt
@@ -1,35 +1,33 @@
 #lang racket
+(require racket/gui/base)
 
-(require (only-in racket/gui/base get-file))
-(require 2htdp/image)
+(define band? (or/c 'red 'green 'blue 0 1 2))
 
-(define band? (or/c 'red 'green 'blue 'alpha 0 1 2 3))
-
-#|
-
-(read-image filepath)
-(write-image image filename)
-
-|#
-
-(provide
- (all-from-out 2htdp/image)
- read-image
- write-image 
+(provide 
+ color-bytes
  (contract-out
+  [color        (-> byte? byte? byte? color?)]
   [color-equal? (-> color? color? boolean?)]
+  [color?       (-> any/c boolean?)]
   [color-ref    (-> color? band? byte?)]
-  [draw-image   (-> image? image?)]
+  [color-set!   (-> color? band? byte? void?)]
+  [print-color  (-> color? color?)]
+  [draw-image   (-> image? void?)]
   [image-cols   (-> image? exact-nonnegative-integer?)]
   [image-rows   (-> image? exact-nonnegative-integer?)]
   [image-equal? (-> image? image? boolean?)]
+  [image?       (-> any/c boolean?)]
   [image-map    (-> (-> color? color?) image? image?)]
-  [image-map-rc (-> (-> exact-nonnegative-integer? exact-nonnegative-integer? color?)
-                    image? image?)]
+  [read-image   (->* () ((or/c path-string? input-port?)) image?)]
+  [write-image  (->* (image?) (path-string?) (or/c image? boolean?))]
   [image-ref  
    (case->
     (-> image? exact-nonnegative-integer? exact-nonnegative-integer? color?)
     (-> image? exact-nonnegative-integer? exact-nonnegative-integer? band? byte?))]
+  [image-set!
+   (case->
+    (-> image? exact-nonnegative-integer? exact-nonnegative-integer? color? void)
+    (-> image? exact-nonnegative-integer? exact-nonnegative-integer? band? byte? void))]
   [make-image
    (case->
     (-> exact-nonnegative-integer? exact-nonnegative-integer? image?)
@@ -45,334 +43,212 @@
   [pink      color?]
   ))
 
-#|
+(define-struct color (bs) #:mutable
+  #:constructor-name color-bytes
+  #:omit-define-syntaxes
+  #:methods gen:custom-write
+         [(define (write-proc color port mode)
+             (define bs (color-bs color))
+             (fprintf
+              port
+              "<color:~a ~a ~a>"
+              (number->string (bytes-ref bs 1))
+              (number->string (bytes-ref bs 2))
+              (number->string (bytes-ref bs 3))))])
 
-C211 Image Library for Racket
-Seth Kirby
+(define (color r g b) (color-bytes (bytes 255 r g b)))
 
-Functions:
-(color r g b alpha)
-(make-color r g b)
-(color? obj)
-(image? obj)
-(scale num img)
-(color-equal? color1 color2)
-(color-ref color band)
-(draw-image image)
-(image-rows image)
-(image-cols image)
-(image-equal? image image)
-(image-map image procedure)
-(image-map-rc image procedure)
-(image-ref image row column)
-(image-ref image row column band)
-(make-image rows cols)
-(make-image rows cols color)
-(make-image rows cols generator)
-(list->image cols ls)
-(read-image filepath)
-(write-image image filename)
-
---------------------------------------------------------------------------------
-Built in from 2htdp/image:
---------------------------------------------------------------------------------
-
-(color r g b alpha)
-=> a color
-
-Creates a new color with red, green, and blue values of r, g, and b
-respectively, and an alpha transparency of alpha.
-
---------------------------------------------------------------------------------
-
-(make-color r g b)
-=> a color
-
-Creates a new color with red, green, and blue values of r, g, and b respectively.
-
---------------------------------------------------------------------------------
-
-(color? obj)
-=> #t or #f
-
-Test if obj is a color.
-
---------------------------------------------------------------------------------
-
-(image? obj)
-=> #t or #f
-
-Test if obj is a image
-
---------------------------------------------------------------------------------
-
-(scale num img)
-=> image
-
-Returns an image that is img scaled by a factor of num.  (scale 2 img) is twice
-as large as image, 0.5 is half as large.
-
---------------------------------------------------------------------------------
-Added by C211 Image Library:
---------------------------------------------------------------------------------
-
-(color-equal? color1 color2)
-=> #t or #f
-
-Test if two colors are equal.
-
-|#
-
-(define (color-equal? c1 c2)
-  (and (= (color-red c1) (color-red c2))
-       (= (color-green c1) (color-green c2))
-       (= (color-blue c1) (color-blue c2))
-       (= (color-alpha c1) (color-alpha c2))))
-
-#|
-
---------------------------------------------------------------------------------
-
-(color-ref color band)
-=> an integer in the range [0, 255]
-
-band should be one of either the symbols red, green, or blue or the
-corresponding integers 0, 1, or 2. Returns the amount
-of that specified color as an integer in the range [0, 255] where 0 is none of
-that color and 255 is the maximum amount.
-
-|#
+(define (color-set! c band val)
+  (case band
+    [(0 red) (bytes-set! (color-bs c) 1 val)]
+    [(1 green) (bytes-set! (color-bs c) 2 val)]
+    [(2 blue) (bytes-set! (color-bs c) 3 val)]))
 
 (define (color-ref c band)
-  (if (integer? band)
-      (cond
-        [(= band 0) (color-red c)]
-        [(= band 1) (color-green c)]
-        [(= band 2) (color-blue c)]
-        [(= band 3) (color-alpha c)])
-      (cond
-        [(equal? band 'red) (color-red c)]
-        [(equal? band 'green) (color-green c)]
-        [(equal? band 'blue) (color-blue c)]
-        [(equal? band 'alpha) (color-alpha c)])))
+  (case band
+    [(0 red) (bytes-ref (color-bs c) 1)]
+    [(1 green) (bytes-ref (color-bs c) 2)]
+    [(2 blue) (bytes-ref (color-bs c) 3)]))
 
-#|
+(define (color-equal? c1 c2)
+  (and (= (color-ref c1 0) (color-ref c2 0))
+       (= (color-ref c1 1) (color-ref c2 1))
+       (= (color-ref c1 2) (color-ref c2 2))))
 
---------------------------------------------------------------------------------
+(define (print-color c)
+  (display "#<color: ")
+  (for ((c (subbytes (color-bs c) 1 4)))
+    (display c) (display " "))
+  (display ">\n")
+  c)
 
-(draw-image image)
-=> image
+(define (draw-image i) (print i)(display "\n"))
 
-Display the image to the REPL.  This is left for legacy compatibility with old
-c211 library code, in racket simply returns the passed image.
+(define (image? i)
+  (or ((is-a?/c bitmap%) i)
+      ((is-a?/c image-snip%) i)))
 
-|#
+(define image-set!
+  (case-lambda
+    [(img r c clr)
+     (define bm
+       (if ((is-a?/c bitmap%) img)
+           img
+           (send img get-bitmap)))
+     (send bm set-argb-pixels c r 1 1
+           (color-bs clr))]
+    [(img r c band value)
+     (define bm
+       (if ((is-a?/c bitmap%) img)
+           img
+           (send img get-bitmap)))
+     (define bytes (make-bytes 4))
+     (send bm get-argb-pixels c r 1 1 bytes)
+     (bytes-set! bytes (case band
+                         [(0 red) 1]
+                         [(1 green) 2]
+                         [(2 blue) 3])
+                 value)
+     (send bm set-argb-pixels c r 1 1 bytes)]))
 
-(define (draw-image x) x)
+(define (image-rows i)
+  (if ((is-a?/c bitmap%) i)
+      (send i get-height)
+      (send (send i get-bitmap) get-height)))
 
-#|
+(define (image-cols i)
+  (if ((is-a?/c bitmap%) i)
+      (send i get-width)
+      (send (send i get-bitmap) get-width)))
 
---------------------------------------------------------------------------------
-
-(image-rows image)
-=> a non-negative integer
-
-Return how many rows are in the image.
-
-|#
-
-(define image-rows image-height)
-
-#|
-
---------------------------------------------------------------------------------
-
-(image-cols image)
-=> a non-negative integer
-
-Return how many columns are in the image.
-
-|#
-
-(define image-cols image-width)
-
-#|
-
---------------------------------------------------------------------------------
-
-(image-equal? image image)
-=> #t or #f
-
-Tests if two images are equal.  Additionally you can use equal?
-
-|#
-
-(define (fast-color-ls-equal? ls1 ls2)
-  (if (null? ls1)
-      #t
-      (if (not (color-equal? (car ls1) (car ls2)))
-      #f
-      (fast-color-ls-equal? (cdr ls1) (cdr ls2)))))
+(define (fast-bitmap-equal? bm1 bm2)
+  (define h (send bm1 get-height))
+  (define w (send bm1 get-width))
+  (define bm1bytes (make-bytes (* 4 h w)))
+  (define bm2bytes (make-bytes (* 4 h w)))
+  (send bm1 get-argb-pixels 0 0 w h bm1bytes)
+  (send bm2 get-argb-pixels 0 0 w h bm2bytes)
+  (define flag #t)
+  (for ((i (in-range 0 (* 4 h w) 4)))
+    #:break (and
+             (not (and (= (bytes-ref bm1bytes (+ i 1)) (bytes-ref bm2bytes (+ i 1)))
+                       (= (bytes-ref bm1bytes (+ i 2)) (bytes-ref bm2bytes (+ i 2)))
+                       (= (bytes-ref bm1bytes (+ i 3)) (bytes-ref bm2bytes (+ i 3)))))
+             (set! flag #f))
+    i)
+  flag)
 
 (define (image-equal? i1 i2)
-  (if (not (and (= (image-cols i1) (image-cols i2))
-                (= (image-rows i1) (image-rows i2))))
-      #f
-      (fast-color-ls-equal? (image->color-list i1) (image->color-list i2))))
-
-#|
-
---------------------------------------------------------------------------------
-
-(image-map procedure image)
-=> an image
-
-Create a new image of the same size as image by mapping a procedure of the form
-(lambda (color) ...)=>color over each pixel in the original.
-
-|#
+  (define bm1
+       (if ((is-a?/c bitmap%) i1)
+           i1
+           (send i1 get-bitmap)))
+  (define bm2
+       (if ((is-a?/c bitmap%) i2)
+           i2
+           (send i2 get-bitmap)))
+  (and (= (send bm1 get-height) (send bm2 get-height))
+       (= (send bm1 get-width) (send bm2 get-width))
+       (fast-bitmap-equal? bm1 bm2)))
 
 (define (image-map func i)
-  (color-list->bitmap
-   (map func (image->color-list i))
-   (image-cols i)
-   (image-rows i)))
-
-#|
-
---------------------------------------------------------------------------------
-
-(image-map-rc procedure image)
-=> an image
-
-Create a new image of the same size as image by mapping a procedure of the form
-(lambda (r c) ...)=>color over each row and column coordinate in the original.
-
-|#
-
-(define (image-map-rc-helper func ls num-rows num-cols r c)
-  (cond
-    [(= r num-rows) '()]
-    [(= c num-cols)
-     (image-map-rc-helper func ls num-rows num-cols (add1 r) 0)]
-    [else
-     (cons
-      (func r c)
-      (image-map-rc-helper func (cdr ls) num-rows num-cols r (add1 c)))]))
-
-(define (image-map-rc func i)
-  (color-list->bitmap
-   (image-map-rc-helper
-    func
-    (image->color-list i)
-    (image-rows i)
-    (image-cols i)
-    0
-    0)
-   (image-cols i)
-   (image-rows i)))
-
-#|
-
---------------------------------------------------------------------------------
-
-(image-ref image row column)
-(image-ref image row column band)
-=> a color or an integer in the range [0, 255]
-
-If band is not specified, access the pixel in image at row x column and return
-the corresponding color.
-
-If band is specified and one of either the symbols red, green, or blue or the
-corresponding integers 0, 1, or 2, then access the pixel in image at row x column
-and return that given band from the pixel as an integer in the range [0, 255].
-
-|#
+  (define bm
+    (if ((is-a?/c bitmap%) i)
+        i
+        (send i get-bitmap)))
+  (define h (send bm get-height))
+  (define w (send bm get-width))
+  (define b (make-bytes (* 4 h w)))
+  (define b2 (make-bytes (* 4 h w)))
+  (send bm get-argb-pixels 0 0 w h b)
+  (for ((i (in-range 0 (* 4 h w) 4)))
+    (bytes-copy!
+     b2
+     i
+     (color-bs (func (color-bytes (subbytes b i (+ i 4)))))
+     0
+     4))
+  (define iout (make-object bitmap% w h))
+  (send iout set-argb-pixels 0 0 w h b2)
+  iout)
 
 (define image-ref
   (case-lambda
-    [(image row col)
-     (list-ref (image->color-list image) (+ (* row (image-cols image)) col))]
-    [(image row col band)
-     (color-ref
-      (list-ref (image->color-list image) (+ (* row (image-cols image)) col))
-       band)]))
-
-#|
-
-
-
---------------------------------------------------------------------------------
-
-(make-image rows cols)
-(make-image rows cols color)
-(make-image rows cols generator)
-=> an image
-
-Create a new rows x cols image, defaulting to a black background.
-
-If color is specified, fill with that color instead.
-
-If generator is specified, it should be a function of the form
-(lambda (r c) ...)=>color where r and c are the coordinates of a specific pixel.
-
-|#
+    [(i r c)
+     (define bm
+       (if ((is-a?/c bitmap%) i)
+           i
+           (send i get-bitmap)))
+     (define b (make-bytes 4))
+     (send bm get-argb-pixels c r 1 1 b)
+     (color-bytes (subbytes b 0 4))]
+    [(i r c band)
+     (define bm
+       (if ((is-a?/c bitmap%) i)
+           i
+           (send i get-bitmap)))
+     (define b (make-bytes 4))
+     (send bm get-argb-pixels c r 1 1 b)
+     (bytes-ref b (case band
+                    [(0 red) 1]
+                    [(1 green) 2]
+                    [(2 blue) 3]))]))
 
 (define make-image
   (case-lambda
-    [(rows cols)
-     (color-list->bitmap (make-list (* rows cols) black)
-                         cols rows)]
-    [(rows cols color/f)
-     (if (color? color/f)
-         (color-list->bitmap (make-list (* rows cols) color/f)
-                             cols rows)
-         (image-map-rc color/f (make-image rows cols)))]))
-
-#|
-
---------------------------------------------------------------------------------
-
-(list->image cols ls)
-=> an image
-
-Create a new image of width cols from a list of colors
-
-|#
-
-(define (list->image width ls)
-  (color-list->bitmap ls width (/ (length ls) width)))
-
-(define image->list
-  (lambda (image)
-    (let loop ([r (sub1 (image-rows image))] [acc '()])
-      (if (negative? r)
-          acc
-          (loop (- r 1)
-            (let loop ([c (sub1 (image-cols image))] [acc acc])
-              (if (negative? c)
-                  acc
-                  (loop (- c 1) (cons (image-ref image r c) acc)))))))))
-
-#|
-
---------------------------------------------------------------------------------
-
-(read-image filepath)
-=> an image
-
-Loads and returns a file as an image.  Can take a relative or complete path.
-Additionally, copied images may be posted directly into racket.
-
-|#
-
-(define read-image
-  (case-lambda
-    [()
+    [(r c)
+     (define b (make-bytes (* 4 r c)))
+     (define blk (bytes 255 0 0 0))
+     (for ((i (in-range 0 (* 4 r c) 4)))
+       (bytes-copy! b i blk 0 4))
+     (define bm (make-object bitmap% c r))
+     (send bm set-argb-pixels 0 0 c r b)
+     bm]
+    [(r c f/color)
      (cond
-       [(get-file "read-image" #f #f #f #f null file-formats)
-        => (lambda (filename) (read-image filename))])]
-    [(filename) (bitmap/file filename)]))
+       [(procedure? f/color)
+        (define b (make-bytes (* 4 r c)))
+        (for ((rr (in-range 0 r)))
+          (for ((rc (in-range 0 c)))
+            (bytes-copy!
+             b
+             (+ (* 4 (* rr c)) (* 4 rc))
+             (color-bs (f/color rr rc))
+             0
+             4)))
+        (define bm (make-object bitmap% c r))
+        (send bm set-argb-pixels 0 0 c r b)
+        bm]
+       [else (define b (make-bytes (* 4 r c)))
+             (define clr (color-bs f/color))
+             (for ((i (in-range 0 (* 4 r c) 4)))
+               (bytes-copy! b i clr 0 4))
+             (define bm (make-object bitmap% c r))
+             (send bm set-argb-pixels 0 0 c r b)
+             bm])]))
+
+(define (list->image c ls)
+  (define r (quotient (length ls) c))
+  (define b (make-bytes (* 4 r c)))
+  (for ((clr ls)(i (in-range 0 (* 4 r c) 4)))
+    (bytes-copy! b i (color-bs clr) 0 4))
+  (define bm (make-object bitmap% c r))
+  (send bm set-argb-pixels 0 0 c r b)
+  bm)
+
+(define (image->list img)
+  (define bm
+       (if ((is-a?/c bitmap%) img)
+           img
+           (send img get-bitmap)))
+  (define h (image-rows bm))
+  (define w (image-cols bm))
+  (define bs (make-bytes (* 4 h w)))
+  (send bm get-argb-pixels 0 0 w h bs)
+  (for/list ((i (in-range 0 (* 4 w h) 4)))
+    (color (bytes-ref bs (+ i 1))
+           (bytes-ref bs (+ i 2))
+           (bytes-ref bs (+ i 3)))))
 
 (define file-formats
   '(("Any image format" "*.png;*.jpg;*.jpeg;*.bmp;*.gif")
@@ -382,27 +258,36 @@ Additionally, copied images may be posted directly into racket.
     ("Graphics interchange format" "*.gif")
     ("Any" "*.*")))
 
+(define read-image
+  (case-lambda
+    [()
+     (cond
+       [(get-file "read-image" #f #f #f #f null file-formats)
+        => (λ (filename) (read-image filename))])]
+    [(filename)
+     (read-bitmap filename)]))
 
-#|
-
---------------------------------------------------------------------------------
-
-(write-image image filename)
-=> #t or #f
-
-Writes and image to the filename path.  Returns boolean of success of save.
-
-|#
-
-(define write-image save-image)
-
-#|
-
---------------------------------------------------------------------------------
-
-Color definitions
-
-|#
+(define write-image
+  (case-lambda
+    [(img)
+     (cond
+       [(get-file "write-image" #f #f #f #f null file-formats)
+        => (λ (filename) (write-image img filename))])]
+    [(img filename)
+     (define bm
+       (if ((is-a?/c bitmap%) img)
+           img
+           (send img get-bitmap)))
+     (define kind 
+       (case (string->symbol (string-downcase (last (string-split filename "."))))
+         [(png)      'png]
+         [(jpg jpeg) 'jpeg]
+         [(xbm)      'xbm]
+         [(xpm)      'xpm]
+         [(bmp)      'bmp]
+         [else
+          (error 'write-image "Cannot write file format, support formats: *.png, *.jpeg, *.xbm, *.xpm, *.bmp")]))
+     (send bm save-file filename kind)]))
 
 (define black     (color 0 0 0))
 (define darkgray  (color 84 84 84))
@@ -416,10 +301,64 @@ Color definitions
 (define cyan      (color 0 255 255))
 (define magenta   (color 255 0 255))
 (define orange    (color 255 127 0))
-(define pink      (color 188 143 143))
+(define pink      (color 188 143 143))   
 
 #|
+(for ((r (in-range 0 100)))
+  (for ((c (in-range 0 100)))
+    (for ((v (in-range 0 3)))
+    (image-set! i2 r c v 125))))
+|
+|#
 
---------------------------------------------------------------------------------
+#|
+(define h (image-height i))
+(define w (image-width i))
+(define bytes (make-bytes (* 4 h w)))
+(define old-bytes (send bm get-argb-pixels 0 0 h w bytes))
+
+(define (image-rc func img)
+  (define h (image-height img))
+  (define w (image-width img))
+  (define bytes (make-bytes (* 4 h w)))
+  (define bm (send i get-bitmap))
+  (define old-bytes (send bm get-argb-pixels 0 0 h w bytes))
+|#
+
+#|
+(define (color-ref color band)
+  (case band
+    [(0 red)   (color-r color)]
+    [(1 green) (color-g color)]
+    [(2 blue)  (color-b color)]
+    [(3 alpha)  (color-a color)]))
+|#
+
+#|
+(define image-set!
+  (case-lambda
+    [(img r c clr)
+     (define bm (send img get-bitmap))
+     (send bm set-argb-pixels c r 1 1
+           (bytes (color-ref clr 3)
+                  (color-ref clr 0)
+                  (color-ref clr 1)
+                  (color-ref clr 2)))]
+    [(img r c band value)
+     (define bm (send img get-bitmap))
+     (define bytes (make-bytes 4))
+     (send bm get-argb-pixels c r 1 1 bytes)
+     (define ref (case band
+                   [(0 red)   1]
+                   [(1 green) 2]
+                   [(2 blue)  3]
+                   [(3 alpha) 0]))
+     (bytes-set! bytes ref value)
+     (send bm set-argb-pixels c r 1 1 bytes)]))
+
+(for ((r (in-range 0 100)))
+  (for ((c (in-range 0 100)))
+    (for ((v (in-range 0 3)))
+    (image-set! i r c v 60))))
 
 |#
